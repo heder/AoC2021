819b07d480a3abf4230e9a0a1e7d95708124ba4e
Thomas Heder <thomas.heder@gmail.com>
2021-11-28 21:43:37 +0100

    init sln

---
+++ AoC2021/Day1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] lines = File.ReadLines("in.txt").Select(f => Convert.ToInt32(f)).ToArray();
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < lines.Length; j++)
+            {
+                if (lines[i] + lines[j] == 2020)
+                {
+                    Console.WriteLine(lines[i] * lines[j]);
+                    Console.ReadKey();
+                    return;
+                }
+            }
+        }
+    }
+}
+
