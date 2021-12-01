ba4d3046ba48791083ee126f76be236954ab1fdc
Thomas Heder <thomas.heder@gmail.com>
2021-12-01 06:38:13 +0100

    D1

--- AoC2021/Day1.1/Program.cs
+++ AoC2021/Day1.1/Program.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
         int[] lines = File.ReadLines("in.txt").Select(f => Convert.ToInt32(f)).ToArray();
 
+        int? prev = null;
+        int noIncreased = 0;
+
         for (int i = 0; i < lines.Length; i++)
         {
-            for (int j = 0; j < lines.Length; j++)
-            {
-                if (lines[i] + lines[j] == 2020)
-                {
-                    Console.WriteLine(lines[i] * lines[j]);
-                    Console.ReadKey();
-                    return;
-                }
-            }
+            if (lines[i] > prev) noIncreased++;
+            prev = lines[i];
         }
+
+        Console.WriteLine(noIncreased);
+        Console.ReadKey();
     }
 }
 
