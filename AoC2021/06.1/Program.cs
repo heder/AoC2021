208be99df0b55720283ed23b8d9a2da68b41acdd
Thomas Heder <thomas.heder@sogeti.com>
2021-12-06 09:15:38 +0100

    9

--- AoC2021/06.1/Program.cs
+++ AoC2021/06.1/Program.cs
@@ -2,11 +2,14 @@
 {
     static void Main()
     {
+        
         List<Fish> fish = File.ReadLines("in.txt").First().Split(',').Select(f => new Fish() { daysLeft = Convert.ToInt32(f) }).ToList();
 
         for (int i = 0; i < 256; i++)
         {
             int toAdd = 0;
+
+            Console.WriteLine($"{i} : {fish.Count()}");
 
             foreach (var item in fish)
             {
@@ -28,74 +31,9 @@
 
         }
 
-
-
         Console.WriteLine(fish.Count);
         Console.ReadKey();
-
-
-
-        //for (int i = 0; i < 18; i++)
-        //{
-
-        //}
-
-
-        //int[,] floor = new int[1000, 1000];
-
-        //foreach (var line in lines)
-        //{
-        //    var coords = line.Split("->").Select(f => f.Trim()).ToArray();
-
-        //    var from = coords[0].Split(',').Select(f => Convert.ToInt32(f)).ToList();
-        //    var to = coords[1].Split(',').Select(f => Convert.ToInt32(f)).ToList();
-
-        //    if (from[0] == to[0]) // x eq
-        //    {
-        //        int yFrom = Math.Min(from[1], to[1]);
-        //        int yTo = Math.Max(from[1], to[1]);
-
-        //        for (int i = yFrom; i <= yTo; i++)
-        //        {
-        //            floor[from[0], i]++;
-        //        }
-        //    }
-        //    else if (from[1] == to[1]) // y eq
-        //    {
-        //        int xFrom = Math.Min(from[0], to[0]);
-        //        int xTo = Math.Max(from[0], to[0]);
-
-        //        for (int i = xFrom; i <= xTo; i++)
-        //        {
-        //            floor[i, from[1]]++;
-        //        }
-        //    }
-        //    else if (Math.Abs(from[0] - to[0]) == Math.Abs(from[1] - to[1])) // 45 deg diag
-        //    {
-        //        int dist = Math.Abs(from[0] - to[0]);
-
-        //        bool xRise = (to[0] >= from[0]);
-        //        bool yRise = (to[1] >= from[1]);
-
-        //        int xcurrent = from[0];
-        //        int ycurrent = from[1];
-
-        //        for (int i = 0; i <= dist; i++)
-        //        {
-        //            floor[xcurrent, ycurrent]++;
-
-        //            if (xRise) xcurrent++; else xcurrent--;
-        //            if (yRise) ycurrent++; else ycurrent--;
-        //        }
-        //    }
-        //}
-
-        //var c = floor.Cast<int>().Count(f => f >= 2);
-
     }
-
-
-
 }
 
 
--- AoC2021/06.2/Program.cs
+++ AoC2021/06.2/Program.cs
@@ -2,112 +2,43 @@
 {
     static void Main()
     {
+        List<int> fish = File.ReadLines("in.txt").First().Split(',').Select(f => Convert.ToInt32(f)).ToList();
+        long[] buckets = new long[9];
 
-        int[] x = new int[26984457539];
-
-        List<Fish> fish = File.ReadLines("in.txt").First().Split(',').Select(f => new Fish() { daysLeft = Convert.ToInt32(f) }).ToList();
-
-        int toAdd = 0;
-
-        for (int i = 0; i < 256; i++)
+        foreach (var item in fish)
         {
-            Console.WriteLine(i);
+            buckets[item]++;
+        }
 
-            toAdd = 0;
+        Console.WriteLine($"initial : fish: {buckets.Sum()}");
 
-            foreach (var item in fish)
+        for (int i = 1; i <= 256; i++)
+        {
+            Console.WriteLine($"iter: {i} : fish: {buckets.Sum()}");
+
+            long toAdd = 0;
+
+            for (int b = 0; b < 9; b++)
             {
-                if (item.daysLeft == 0)
+                if (b == 0)
                 {
-                    toAdd++;
-                    item.daysLeft = 6;
+                    toAdd = buckets[0];
+                    buckets[0] = 0;
                 }
                 else
                 {
-                    item.daysLeft--;
+                    buckets[b - 1] += buckets[b];
+                    buckets[b] = 0;
                 }
             }
 
-            for (int j = 0; j < toAdd; j++)
-            {
-                fish.Add(new Fish() { daysLeft = 8 });
-            }
-
+            buckets[6] += toAdd;
+            buckets[8] += toAdd;
         }
 
-
-
-        Console.WriteLine(fish.Count);
+        var c = buckets.Sum(f => f);
+        Console.WriteLine(c);
         Console.ReadKey();
-
-
-
-        //for (int i = 0; i < 18; i++)
-        //{
-
-        //}
-
-
-        //int[,] floor = new int[1000, 1000];
-
-        //foreach (var line in lines)
-        //{
-        //    var coords = line.Split("->").Select(f => f.Trim()).ToArray();
-
-        //    var from = coords[0].Split(',').Select(f => Convert.ToInt32(f)).ToList();
-        //    var to = coords[1].Split(',').Select(f => Convert.ToInt32(f)).ToList();
-
-        //    if (from[0] == to[0]) // x eq
-        //    {
-        //        int yFrom = Math.Min(from[1], to[1]);
-        //        int yTo = Math.Max(from[1], to[1]);
-
-        //        for (int i = yFrom; i <= yTo; i++)
-        //        {
-        //            floor[from[0], i]++;
-        //        }
-        //    }
-        //    else if (from[1] == to[1]) // y eq
-        //    {
-        //        int xFrom = Math.Min(from[0], to[0]);
-        //        int xTo = Math.Max(from[0], to[0]);
-
-        //        for (int i = xFrom; i <= xTo; i++)
-        //        {
-        //            floor[i, from[1]]++;
-        //        }
-        //    }
-        //    else if (Math.Abs(from[0] - to[0]) == Math.Abs(from[1] - to[1])) // 45 deg diag
-        //    {
-        //        int dist = Math.Abs(from[0] - to[0]);
-
-        //        bool xRise = (to[0] >= from[0]);
-        //        bool yRise = (to[1] >= from[1]);
-
-        //        int xcurrent = from[0];
-        //        int ycurrent = from[1];
-
-        //        for (int i = 0; i <= dist; i++)
-        //        {
-        //            floor[xcurrent, ycurrent]++;
-
-        //            if (xRise) xcurrent++; else xcurrent--;
-        //            if (yRise) ycurrent++; else ycurrent--;
-        //        }
-        //    }
-        //}
-
-        //var c = floor.Cast<int>().Count(f => f >= 2);
-
     }
-
-
-
-}
-
-
-class Fish
-{
-    public int daysLeft { get; set; }
 }
 
