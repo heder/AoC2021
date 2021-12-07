246bae9239c5c0f61885d78b4426ce418bac995a
Thomas Heder <thomas.heder@gmail.com>
2021-12-07 06:53:22 +0100

    7

---
+++ AoC2021/07.1/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<int> crabs = File.ReadLines("in.txt").First().Split(',').Select(f => Convert.ToInt32(f)).ToList();
+
+        var lbound = crabs.Min();
+        var rbound = crabs.Max();
+
+        // bf
+        int consumption;
+        int lowest = int.MaxValue;
+        for (int i = lbound; i <= rbound; i++)
+        {
+            consumption = 0;
+
+            foreach (var crab in crabs)
+            {
+                consumption += Math.Abs(crab - i);
+            }
+
+            lowest = Math.Min(consumption, lowest);
+        }
+
+        Console.WriteLine(lowest);
+        Console.ReadKey();
+    }
+}
---
+++ AoC2021/07.2/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<int> crabs = File.ReadLines("in.txt").First().Split(',').Select(f => Convert.ToInt32(f)).ToList();
+
+        var lbound = crabs.Min();
+        var rbound = crabs.Max();
+
+        // bf
+        int consumption;
+        int lowest = int.MaxValue;
+        for (int i = lbound; i <= rbound; i++)
+        {
+            consumption = 0;
+
+            foreach (var crab in crabs)
+            {
+                int dist = Math.Abs(crab - i);
+                for (int c = 1; c <= dist; c++)
+                {
+                    consumption += c;
+                }
+            }
+
+            lowest = Math.Min(consumption, lowest);
+        }
+
+        Console.WriteLine(lowest);
+        Console.ReadKey();
+    }
+}
